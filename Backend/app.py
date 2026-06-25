c1dd8c2687728b0c67253098f2affe1a11503815
Rimzhim <Rimzhim0911@gmail.com>
2026-06-25 18:57:48 +0530

    Fix CORS

--- Backend/app.py
+++ Backend/app.py
@@ -6,8 +6,17 @@ import numpy as np
 from PIL import Image
 
 app = Flask(__name__)
-CORS(app)
 
+CORS(
+    app,
+    resources={
+        r"/*": {
+            "origins": [
+                "https://mango-leaf-app.vercel.app"
+            ]
+        }
+    }
+)
 
 @app.route("/")
 def home():
